55bf8f799558b815b1c9b0b07555bfed5b8d155a
StylishBoi <max@rdh.ch>
2025-05-28 14:30:16 +0200

    -Pre-Deleted test_angle 'instantiate' code delete

--- core/include/maths/angle.h
+++ core/include/maths/angle.h
@@ -3,34 +3,42 @@
 
 #include "maths/const.h"
 
-namespace core {
+namespace core::maths {
+//States/Initializes the class beforehand so it can be used within radian without causing an error
 class Degree;
 class Radian {
 public:
-  constexpr explicit Radian(float value): value_(value){}
+  //Needs to be explicit and state that it is float
+  constexpr explicit Radian(float value): radian_value_(value){}
+  //No need to be explicit if the value is already a degree
   constexpr Radian(const Degree& angle);
 
-  explicit constexpr operator float() const { return value_; }
+  //Converts the radian into a float value
+  explicit constexpr operator float() const { return radian_value_; }
 
 private:
-  float value_;
+  //The value of the radian of the object
+  float radian_value_;
 };
 
 class Degree {
 public:
-  constexpr explicit Degree(float value): value_(value){}
-  constexpr Degree(const Radian& angle): value_(static_cast<float>(angle)/Pi*180.0f) {}
-  explicit constexpr operator float() const {
-    return value_;
-  }
+  //Needs to be explicit and state that it is float
+  constexpr explicit Degree(float value): degree_value_(value){}
+  //No need to be explicit if the value is already a radian
+  constexpr Degree(const Radian& angle): degree_value_(static_cast<float>(angle)/Pi*180.0f) {}
+
+  //Converts the degree into a float value
+  explicit constexpr operator float() const { return degree_value_; }
 
 private:
-  float value_;
+  //The value of the degree of the object
+  float degree_value_;
 };
 
 
 constexpr Radian::Radian(const Degree& angle)
-    : value_(static_cast<float>(angle) / 180.0f * Pi) {}
+    : radian_value_(static_cast<float>(angle) / 180.0f * Pi) {}
 }
 
 #endif
--- core/test/test_angle.cc
+++ core/test/test_angle.cc
@@ -1,11 +1,26 @@
+#include <SFML/Window/ContextSettings.hpp>
+
 #include "gtest/gtest.h"
 #include "maths/angle.h"
 #include "maths/func.h"
 
+/*struct AngleOperationFixture : public ::testing::TestWithParam<float>
+{
+};*/
+
 TEST(Vec2f, Conversion) {
-  core::Degree d2 = 45.0f;
-  core::Degree degree{45.0f};
-  auto result = core::Cos(static_cast<core::Radian>(degree));
-  core::Sin(45.0f);
+  core::Degree test_degree_{45.0f};
+
+  auto result = core::Cos(static_cast<core::Radian>(test_degree_));
+
   EXPECT_FLOAT_EQ(result, std::cos(core::Pi/4.0f));
 }
+
+/*INSTANTIATE_TEST_SUITE_P(AllNumbers,
+                         AngleOperationFixture,
+                         testing::Values(
+                           2.0f,
+                           -2.0f,
+                           0.0f
+                         )
+);*/
